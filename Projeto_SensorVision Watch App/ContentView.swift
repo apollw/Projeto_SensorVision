923ca317e4478c7e5bd29d6c7bc2948f0523b581
apollw <apollw@gmail.com>
2024-02-29 14:43:36 -0300

    Initial Commit

---
+++ Projeto_SensorVision Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projeto_SensorVision Watch App
+//
+//  Created by Fábrica de Inovacao on 29/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projeto_SensorVision Watch App/Projeto_SensorVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projeto_SensorVisionApp.swift
+//  Projeto_SensorVision Watch App
+//
+//  Created by Fábrica de Inovacao on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Projeto_SensorVision_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
